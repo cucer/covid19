036f11e7298afb80ae435541a1d0f2cc3b5d9702
Cagatay Ucer <cagatay.ucer@gmail.com>
2021-01-14 13:06:48 +0300

    Updates

---
+++ precache-manifest.fd44f1934985e2121c4e38de0e9e71e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdaa6452ec446d02418c486d0ece8651",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "8dbff85d7f3f5786a802",
+    "url": "/covid19/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "dedc9024bcc524ca66b8",
+    "url": "/covid19/static/css/main.1be3c61c.chunk.css"
+  },
+  {
+    "revision": "8dbff85d7f3f5786a802",
+    "url": "/covid19/static/js/2.704aeb89.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid19/static/js/2.704aeb89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dedc9024bcc524ca66b8",
+    "url": "/covid19/static/js/main.ff5ba940.chunk.js"
+  },
+  {
+    "revision": "ef7f03afefd82f00647a",
+    "url": "/covid19/static/js/runtime-main.dda563dc.js"
+  }
+]);
